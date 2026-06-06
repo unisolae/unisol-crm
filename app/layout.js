80680030af07c9406fa0d6df128c04f832c9cbaa
unisolae <van@unisol.gr>
2026-06-06 23:41:26 +0300

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="el" className={`${display.variable} ${body.variable}`}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.5.0/dist/tabler-icons.min.css"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
